b33ce2b0f06be3ebfea0feb9a4fb5a1df65bc9b7
Marco Köpcke <marco.koepcke@tudock.de>
2021-12-04 18:15:08 +0100

    Fix wheel again

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup, find_packages
 setup(
     name='skytemple-3rdparty-typestubs',
     version=__version__,
-    packages=find_packages(),
-    package_data={'ndspy': ['py.typed']},
+    packages=['ndspy'],
+    package_data={'ndspy': ['py.typed', '*.pyi']},
     description='(Minimal) PEP 561 type stubs for libraries SkyTemple uses',
     url='https://github.com/SkyTemple/skytemple_/',
     classifiers=[
