3d9ef8b36268d8f0969efa69d46e043fb49bccc3
Marco "Capypara" Köpcke <hello@capypara.de>
2024-08-04 18:48:52 +0200

    fix NintendoDSRom.save return type

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
-__version__ = '1.1.0'
+__version__ = "1.1.1"
 
 from setuptools import setup
 
 setup(
-    name='skytemple-3rdparty-typestubs',
+    name="skytemple-3rdparty-typestubs",
     version=__version__,
-    packages=['ndspy', 'igraph'],
-    package_data={
-        'igraph': ['py.typed', '*.pyi'],
-        'ndspy': ['py.typed', '*.pyi']
-    },
-    description='(Minimal) PEP 561 type stubs for libraries SkyTemple uses',
-    url='https://github.com/SkyTemple/3rdparty-typestubs/',
+    packages=["ndspy", "igraph"],
+    package_data={"igraph": ["py.typed", "*.pyi"], "ndspy": ["py.typed", "*.pyi"]},
+    description="(Minimal) PEP 561 type stubs for libraries SkyTemple uses",
+    url="https://github.com/SkyTemple/3rdparty-typestubs/",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
     ],
 )
